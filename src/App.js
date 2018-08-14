66fdba9aadc182ed2de98e8dc3d9877c9b452ac6
alisson_coj <alisson.coj@hotmail.com>
2018-08-14 01:42:32 -0300

    adicionando outros componentes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import HelloWorld from './components/HelloWorld/HelloWorld';
-import Text from './components/WelcomeText/Text';
+import Header from './components/header'
+import TaskBar from './components/taskbar';
+import TaskList from './components/tasklist';
 import './App.css';
 
+import HelloWorld from './components/HelloWorld/HelloWorld';
+import Text from './components/WelcomeText/Text';
+import ConfirmButton from './components/Buttons/ConfirmButton';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <HelloWorld />
-        <Text />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<TaskBar />
+				<TaskList />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers/'
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,
+    document.getElementById('root')
+);
 registerServiceWorker();
